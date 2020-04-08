32f639458a78b080ad5d8cc6e67936ed60cc112f
hyeonjae Lee <odyflame@icloud.com>
2020-04-08 17:51:11 +0900

    update my ARKit.. i learn about ar object with gestureRecognizer..

---
+++ SimpleBox/SimpleBox/Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  Extensions.swift
+//  SimpleBox
+//
+//  Created by apple on 2020/04/08.
+//  Copyright © 2020 odyflame. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension CGFloat {
+    static func random() -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+}
+
+extension UIColor {
+    static func random() -> UIColor {
+        return UIColor(red: .random(), green: .random(), blue: .random(), alpha: 1.0)
+    }
+}
--- SimpleBox/SimpleBox/ViewController.swift
+++ SimpleBox/SimpleBox/ViewController.swift
@@ -25,20 +25,39 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a new scene
         let scene = SCNScene()
-        
+            
         // Set the scene to the view
         sceneView.scene = scene
         
-        let box = SCNBox(width: 0.3, height: 0.3, length: 0.3, chamferRadius: 0.3)
+        let box = SCNBox(width: 0.3, height: 0.3, length: 0.3, chamferRadius: 0)
         let material = SCNMaterial() //some color can wrap your quve
         material.diffuse.contents = UIColor.red//materail은 한 면이고 그 면은 빨간색이다.
-        
+        material.name = "Color"
+         
         box.materials = [material]//이미지들을 모두 빨간색로 바꾼다. 박스는 6가지의 면이 있으니 6개의 면이 바뀔 것이다.
         
         let boxNode = SCNNode(geometry: box)//박스를 어디로 놔둘 것인지 노드를 정하는것
         boxNode.position = SCNVector3(0, 0, 0)//위치
         
         self.sceneView.scene.rootNode.addChildNode(boxNode)
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func tapped(recognizer: UIGestureRecognizer) {
+        
+        let sceneView = recognizer.view as! SCNView
+        let touchLocation = recognizer.location(in: sceneView)
+        
+        let hitResult = sceneView.hitTest(touchLocation, options: [:])
+        if !hitResult.isEmpty {
+            let node = hitResult[0].node
+            let material = node.geometry?.material(named: "Color")
+            
+            material?.diffuse.contents = UIColor.random()
+        }
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
